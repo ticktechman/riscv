4c57f82b59cb7dc70466e03a4cb34ecc61188dba
ticktechman <geek.wystan@gmail.com>
2026-08-30 22:50:55 +0800

    style: hex.sh warning

--- sim/hex.sh
+++ sim/hex.sh
@@ -9,12 +9,13 @@
 ###############################################################################
 
 rm -f ./riscv-tests/{*.hex,*.data}
-for one in $(ls -1 ./riscv-tests/rv64*); do
+
+for one in ./riscv-tests/rv64*; do
   echo "> $one"
   riscv-none-elf-objcopy -O verilog --verilog-data-width=1 -j .text.init --change-section-address .text.init=0 $one $one.hex >/dev/null
   riscv-none-elf-objcopy -O verilog --verilog-data-width=1 -j .data --change-section-address .data=0 --no-change-warnings $one $one.data >/dev/null
 done
 
-find ./riscv-tests -size 0 | xargs rm -f
+find ./riscv-tests -size 0 -print0 | xargs -0 rm -f
 
 ###############################################################################
